5f9dc83af015e0a424670b07ce4a5419cf506e76
dimasrifalta <dimasrifalta@gmail.com>
2023-06-24 19:19:37 +0700

    finish 2. Developing Your First Microservice - Catalog.API with MongoDb

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -1,6 +1,10 @@
 ﻿using Catalog.API.Entities;
 using Microsoft.Extensions.Configuration;
 using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Catalog.API.Data
 {
@@ -8,12 +12,13 @@ namespace Catalog.API.Data
     {
         public CatalogContext(IConfiguration configuration)
         {
-            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionStrings"));
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
             var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
             Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
             CatalogContextSeed.SeedData(Products);
         }
-        public IMongoCollection<Product> Products { get; }
 
+        public IMongoCollection<Product> Products { get; }
     }
 }
